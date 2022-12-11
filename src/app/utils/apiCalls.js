b1d51328cfb956fbb2d2d57fddb5f3f5827661cc
barakformanski <barakformanski@gmail.com>
2022-12-11 20:03:30 +0200

    change to https

--- src/app/utils/apiCalls.js
+++ src/app/utils/apiCalls.js
@@ -2,11 +2,11 @@ import axios from "axios";
 export const apiCall = async (urlPeriod, query, name, metric, key) => {
   let url;
   if (name === "fiveDaysForecast") {
-    url = `http://dataservice.accuweather.com/${urlPeriod}?apikey=${process.env.REACT_APP_API_KEY}&metric=${metric}`;
+    url = `https://dataservice.accuweather.com/${urlPeriod}?apikey=${process.env.REACT_APP_API_KEY}&metric=${metric}`;
   } else if (name === "autocomplete") {
-    url = `http://dataservice.accuweather.com/${urlPeriod}?apikey=${process.env.REACT_APP_API_KEY}${query}`;
+    url = `https://dataservice.accuweather.com/${urlPeriod}?apikey=${process.env.REACT_APP_API_KEY}${query}`;
   } else if (name === "currentconditions") {
-    url = `http://dataservice.accuweather.com/${urlPeriod}/${key}?apikey=${process.env.REACT_APP_API_KEY}`;
+    url = `https://dataservice.accuweather.com/${urlPeriod}/${key}?apikey=${process.env.REACT_APP_API_KEY}`;
     const response = await axios.get(url);
     let resData = response.data;
     resData[0].key = key;
